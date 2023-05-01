801839f01648281ae037e31c9489126acdad0369
Nishanth S <79076900+nishanthss@users.noreply.github.com>
2023-05-01 19:07:36 +0530

    Delete app1 directory

--- app1/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Query(models.Model):
-    useremail = models.CharField(max_length=50)
-    username = models.CharField(max_length=100)
-    userquery = models.TextField(default="NULL")
